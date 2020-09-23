68b8e909d2b39c902d0968928ceb8ee6610ba3d8
Chris Dahlberg <csdahlberg@gmail.com>
2020-09-23 10:35:32 -0500

    Added SingleReturnReproFx

---
+++ tests/SingleReturnRepro/SingleReturnReproFx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleReturnReproFx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = string.Concat("Hello", ", ", "World!");
+            Console.WriteLine(message);
+
+            Console.WriteLine();
+            Console.WriteLine($"Is64BitProcess: {Environment.Is64BitProcess}");
+
+            var envVars = Environment.GetEnvironmentVariables();
+            foreach (string envVarKey in envVars.Keys.OfType<string>())
+            {
+                if (envVarKey.Contains("COR") || envVarKey.Contains("Microsoft"))
+                {
+                    Console.WriteLine($"{envVarKey}={envVars[envVarKey]}");
+                }
+            }
+        }
+    }
+}
